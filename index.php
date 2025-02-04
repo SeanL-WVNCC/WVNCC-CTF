9c42e6f07d3b77484290a49973773720ee61f38d
Sean Lauritzen <seanl@desktop.lauritzenwifi.net>
2025-02-04 16:07:50 -0500

    Fixed path traversal attack

--- index.php
+++ index.php
@@ -3,8 +3,26 @@
     <?php include "include/head.php" ?>
     <body>
         <?php include "include/header.php" ?>
-        <!-- INSECURE: this code is vulnerable to a path traversal attack. -->
-        <?php include $_GET["page"] ?>
+         <?php
+
+            $includeDirectory = "/var/www/html";
+            $isVulnerableToPathTraversal = True; // Modify this to enable/disable path traversal attack
+            $pageToInclude = $_GET["page"];
+            if($isVulnerableToPathTraversal) {
+                // If path traversal is possible, divulge in comment
+                echo "<!-- INSECURE: this code is vulnerable to a path traversal attack. -->";
+            }
+            if(file_exists($pageToInclude))  {
+                $pageToInclude = realpath($pageToInclude);
+                // Make sure the included folder is in the "includes" folder... sometimes
+                if(str_starts_with($pageToInclude, $includeDirectory) or $isVulnerableToPathTraversal) {
+                    include $pageToInclude;
+                }
+            } else {
+                echo "<main></main>";
+            }
+
+         ?>
         <?php include "include/footer.php" ?>
     </body>
 </html>
