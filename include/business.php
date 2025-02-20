5ad7cc92bc3e018a9f7d0c47034602fbb7a339c7
aackerman82 <ackman1217@gmail.com>
2025-02-20 14:54:19 -0500

    Update business.php - medium security vuln

--- include/business.php
+++ include/business.php
@@ -10,4 +10,19 @@
             Lorem ipsum odor amet, consectetuer adipiscing elit. Viverra commodo tempus facilisis leo cras vivamus fusce. Donec leo interdum nullam aptent cubilia ac nunc.
         </p>
     </section>
+    <!--Medium security search-->
+    <section id="business-search">
+        <form method="POST">
+        <h2>Search 🔎</h2>
+            <input type="text" id="med-search" name="med-search" required>
+            <input type="submit" id="med-submit" name="med-submit" value="Search">
+        </form>
+        <?php 
+        if (isset($_POST["med-submit"])) {
+            $search = htmlspecialchars($_POST["med-search"]);
+            $result = "Search Result: " . $search;
+            echo $result;
+        }
+        ?>
+    </section>
 </main>
