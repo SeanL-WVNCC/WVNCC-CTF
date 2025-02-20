8d68e2bb72fd711bc6f33a9044259fa3f4ba4c92
aackerman82 <ackman1217@gmail.com>
2025-02-20 14:51:52 -0500

    Updated team section

--- include/about.php
+++ include/about.php
@@ -3,7 +3,12 @@
         <h2>Our story</h2>
         <p>Northern Phish &amp; Loan is a bank that has been commited to serving the Ohio Valley since 1953.</p>
         <h2>Meet Our Team!</h2>
-        <p>We are still getting our titles together. Bear with us! </p>
+        <p>Angela Ackermann - Director of Threatening SLAPP Lawsuits</p>
+        <p>Grant Kent - Director of Incompliance</p>
+        <p>Josephine Poulin - Vice President of Elaborate Documentation and Auditing</p>
+        <p>Kevin Hoge - Chief Operator of Perimeter Doorknobs</p>
+        <p>Sean Lauritzen II - Associate Director of Operational Strategy Management</p>
+        <p>Wyatt McNeil - ROLE NEEDED</p>
         <h2>Contact</h2>
         <p><a href="tel:304-555-1234">Call Us: 304-555-1234</a></p>
         <p>Our Address: 1234 Bank Street</p>
