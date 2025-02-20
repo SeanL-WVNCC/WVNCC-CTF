e16fc8f10fe9e402e2d02a33e4a38c757b704da6
aackerman82 <ackman1217@gmail.com>
2025-02-20 14:57:14 -0500

    Update financial-education.php - zero security search
    I started working on these before seeing the search-xss branch, so I thought it'd be best to put these separate from that branch! I'm sure we can combine them so they work together or change mine entirely, I'm still learning this - working on the higher security version but I need to research a bit more.

--- include/financial-education.php
+++ include/financial-education.php
@@ -5,4 +5,18 @@
             Etiam semper quisque pulvinar volutpat hendrerit consectetur. Convallis auctor finibus eleifend orci aptent felis. Mi litora ridiculus; suspendisse nostra parturient nisi mattis. Porta pharetra tempor accumsan fermentum per ligula metus. 
             Lorem aliquet cras felis nascetur phasellus odio. Sapien mus penatibus, aliquam iaculis cras imperdiet. Montes vel condimentum nunc penatibus egestas turpis porttitor.</p>
     </section>
+    <!--Zero security search / XSS if I understand correctly -Angela-->
+    <section id="financial-search">
+        <form method="POST">
+        <h2>Search 🔎</h2> <!--Feel free to remove these emojis if either of you want to, I put it there for fun -Angela-->
+            <input type="text" id="insecure-search" name="insecure-search" required>
+            <input type="submit" id="insecure-submit" name="insecure-submit" value="Search">
+        </form>
+        <?php 
+        if (isset($_POST["insecure-submit"])) {
+            $search = "Search Result: " . $_POST["insecure-search"];
+            echo $search;
+        }
+        ?>
+    </section>
 </main>
