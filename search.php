a26f92d4a2cc255dca5e8e3d67168846c081b05d
aackerman82 <ackman1217@gmail.com>
2025-02-25 12:50:07 -0500

    Update search.php - text blended into background

--- search.php
+++ search.php
@@ -14,7 +14,16 @@
                     } else {
                         $valueToDisplay = htmlspecialchars($_GET["query"]);
                     }
-                    echo "<p>No results found for \"".$valueToDisplay."\"</p>";
+                    //Two different ways of hiding the result in the background
+
+                    //og echo without the class just for testing if needed:
+                    //echo "<p>No results found for \"".$valueToDisplay."\"</p>";
+
+                    /*Colors all the text the same as the body background color
+                    echo "<p class='hidden-search'>No results found for \"".$valueToDisplay."\"</p>";*/
+
+                    //Only colors the result itself
+                    echo "<p>No results found for <span style='color:#FCECE5'>\"".$valueToDisplay."\"</span></p>";
                 }
                 echo "</main>";
             ?>
