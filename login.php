748c0bbadc8f82095f741908ace88b72864e6575
Sean Lauritzen <seanl@laptop.lauritzenwifi.net>
2025-02-27 19:19:20 -0500

    User enumeration

--- login.php
+++ login.php
@@ -9,28 +9,30 @@
                 include "include/vulnconfig.php";
                 $output = "";
                 $output .= "<main id=\"main\">";
+                $output .= "<form aria-labelledby=\"login-heading\" method=\"POST\" action=\"login.php\">";
+                $output .= "<h2 id=\"login-heading\">Login</h2>";
+                $output .= "<label for=\"username-field\">Username</label>";
+                $output .= "<input id=\"username-field\" type=\"text\" name=\"username\" autofocus required>";
+                $output .= "<label for=\"password-field\">Password</label>";
+                $output .= "<input id=\"password-field\" type=\"password\" name=\"password\" required>";
+                $output .= "<button type=\"submit\">Login</button>";
                 if($_SERVER['REQUEST_METHOD'] == "POST") {
                     $conn = mysqli_connect("db", "root", "hackme", "breakTheBank");
                     $username = $_POST["username"];
                     $password = $_POST["password"];
-                    #echo "SELECT * FROM users WHERE username=\"$username\" AND password=\"$password\"";
-                    $result = $conn->query("SELECT * FROM users WHERE username=\"$username\" AND password=\"$password\"");
-                    if($result->fetch_assoc()) {
-                        $output .= "Username and password were correct.";
+                    $result = $conn->query("SELECT * FROM users WHERE username=\"$username\"");
+                    if($user = $result->fetch_assoc()) {
+                        if($password == $user["password"]) {
+                            $output .= "Username and password were correct.";
+                        } else {
+                            $output .= "Password incorrect.";
+                        }
                     } else {
-                        $output .= "Login failure.";
+                        $output .= "Username \"$username\" not found.";
                     }
                     #setcookie("is-logged-in", "true", path:"/");
-                } else {
-                    $output .= "<form aria-labelledby=\"login-heading\" method=\"POST\" action=\"login.php\">";
-                    $output .= "<h2 id=\"login-heading\">Login</h2>";
-                    $output .= "<label for=\"username-field\">Username</label>";
-                    $output .= "<input id=\"username-field\" type=\"text\" name=\"username\" autofocus required>";
-                    $output .= "<label for=\"password-field\">Password</label>";
-                    $output .= "<input id=\"password-field\" type=\"password\" name=\"password\" required>";
-                    $output .= "<button type=\"submit\">Login</button>";
-                    $output .= "</form>";
                 }
+                $output .= "</form>";
                 $output .= "</main>";
                 echo $output;
             ?>
