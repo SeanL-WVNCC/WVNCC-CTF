c08cc1f977c686a9d4dc1af0be992953280de4c1
Sean Lauritzen <seanl@desktop.lauritzenwifi.net>
2025-03-02 15:46:42 -0500

    Removed old hostname reference

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
         <div>
             <?php include 'include/secondary-nav.php';?>
              <?php
-                $conn = mysqli_connect("html-db-1", "root", "hackme");
                 $includeDirectory = "/var/www/html";
                 $isVulnerableToPathTraversal = True; // Modify this to enable/disable path traversal attack
                 $pageToInclude = $_GET["page"];
