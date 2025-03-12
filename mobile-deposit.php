572baabc1f843a8f5c073b35693408b287d13d9d
WyattMcNeil <wyattmcneil3@outlook.com>
2025-03-12 13:55:01 -0400

    Added Upload.php and Uploads folder for DDOS and Insecure File upload Added new branch to not mess up any current organization

---
+++ mobile-deposit.php
@@ -0,0 +1,10 @@
+<main id="main">
+    <p>Upload checks for deposit here. Only image files can be submitted.</p>
+    <form aria-label="Mobile check deposit" action="upload.php" method="post" enctype="multipart/form-data">
+        Please Upload Photo of check: 
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+
+    </form>
+</main>
