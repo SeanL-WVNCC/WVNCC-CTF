8746869f40521cf7920de6e93132f2b770fdb9d1
Sean Lauritzen <seanl@desktop.lauritzenwifi.net>
2025-04-03 17:47:23 -0400

    Style change to feedback

--- feedback.php
+++ feedback.php
@@ -1,16 +1,15 @@
 <?php
 include "include/functions.php";
-$mainContent = "<section id=\"hero-section\">";
-$mainContent .= "<hgroup>";
-$mainContent .= "<h2 id=\"hero-section-title\">Customer Feedback</h2>";
-$mainContent .= "<p>We value your opinion!</p>";
-$mainContent .= "</hgroup>";
-$mainContent .= "<img src=\"img/review.jpg\" alt=\"\">";
-$mainContent .= "</section>";
-$mainContent .= "<section id=\"feedbackField\">";
+$banner = "<section id=\"hero-section\">";
+$banner .= "<hgroup>";
+$banner .= "<h2 id=\"hero-section-title\">Customer Feedback</h2>";
+$banner .= "<p>We value your opinion!</p>";
+$banner .= "</hgroup>";
+$banner .= "<img src=\"img/review.jpg\" alt=\"\">";
+$banner .= "</section>";
+$mainContent .= "<section id=\"feedbackField\" class=\"single-column\">";
 $mainContent .= "<form aria-labelledby=\"send-feedback-heading\" method=\"POST\", action='feedback.php' id='reviewSubmit'>";
 $mainContent .= "<h2 id=\"send-feedback-heading\">We love to hear from our customers! Feel free to leave us some feedback!</h2>";
-$mainContent .= "<!-- Hidden fields, please do not tamper -->";
 $mainContent .= "<input id=\"username-field\" type=\"hidden\" name=\"username\" required>";
 $mainContent .= "<input id=\"date-field\" type=\"hidden\" name=\"date\" required>";
 $mainContent .= "<textarea id=\"feedback\" name=\"feedback\" rows=\"5\" form=\"reviewSubmit\" autofocus required>";
@@ -32,13 +31,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $reviewData = "<li id='reviewSubmissions'><b><u>" . $user['username'] . "</u></b><br>" . $feedback . "<br>"  . $date;
     file_put_contents("reviews/Reviews.txt", $reviewData, FILE_APPEND);}
 if (is_file("reviews/Reviews.txt")){
-    $reviews = file_get_contents("reviews/Reviews.txt");}
+    $reviews = "<div class=\"single-column\">" . file_get_contents("reviews/Reviews.txt") . "</div>";}
 else{
     $reviews = " ";
 }
 
-
-// No need for a clear fields input
 $mainContent .= "</form>" . $reviews;
-echo generatePage($mainContent);
+echo generatePage($banner . $mainContent);
 
