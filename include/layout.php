fa2cee29675c7a4082587418d99e39a8ba56a0b0
Sean Lauritzen <seanl@desktop.lauritzenwifi.net>
2025-04-19 17:35:28 -0400

    More site content

--- include/layout.php
+++ include/layout.php
@@ -45,3 +45,26 @@ function createBanner(string $title, string $subtitle, string $backgroundImageUr
 
     return $html;
 }
+
+/**
+ * Returns HTML for a newsletter article.
+ * @param string $title
+ * @param Date $date
+ * @param string $content
+ * @return string
+ */
+function createNewsletterArticle(string $title, DateTime $date, string $content) {
+
+    $html = "";
+    $html .= "<article class=\"h-entry\">";
+    $html .= "<hgroup>";
+    $html .= "<h3 class=\"p-name\">$title</h3>";
+    $dateIso = date_format($date, "Y-m-d");
+    $dateUsa = date_format($date, "F jS, Y");
+    $html .= "<p><date datetime=\"$dateIso\">$dateUsa</date></p>";
+    $html .= "</hgroup>";
+    $html .= "<div class=\"e-content\">$content</div>";
+    $html .= "</article>";
+
+    return $html;
+}
